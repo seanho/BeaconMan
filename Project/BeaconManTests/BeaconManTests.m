5c95c28a693996e6fe88c2f49ea9a9a2e8100d78
Sean Ho <seanho@thoughtworks.com>
2014-02-18 14:38:26 +1100

    Bootstrap project

---
+++ Project/BeaconManTests/BeaconManTests.m
@@ -0,0 +1,34 @@
+//
+//  BeaconManTests.m
+//  BeaconManTests
+//
+//  Created by Sean Ho on 18/02/2014.
+//  Copyright (c) 2014 Sean Ho. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BeaconManTests : XCTestCase
+
+@end
+
+@implementation BeaconManTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
